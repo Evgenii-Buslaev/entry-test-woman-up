e119f74c16ae4a950db0a050aa3981190b0bd502
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-11-19 10:33:11 +0500

    Implemented openned state in TodoItems, created toggleBtn, added respons. styles

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -9,21 +9,35 @@ import {
 
 import removeBtn from "../../assets/icons/delete.png";
 import doneBtn from "../../assets/icons/done.png";
+import expandBtn from "../../assets/icons/expand.png";
+import turnBtn from "../../assets/icons/turn.png";
 
 import styles from "../../css/components/TodoItem/TodoItem.module.css";
 
 const TodoItem = ({ data, store }) => {
   const { id, task, date, done } = data;
   const [title, setTitle] = useState(task);
+  const [openned, setOpenned] = useState(false);
+
+  console.log(openned);
 
   return (
     <div className={done ? `${styles.item} ${styles.done}` : styles.item}>
-      <input
-        className={styles.text}
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        onBlur={(e) => editTodoTitle(id, e.target.value, store, setTitle)}
-      ></input>
+      <div className={styles.firstLine}>
+        <img
+          className={styles.openClose}
+          title={openned ? "Свернуть" : "Раскрыть"}
+          src={openned ? turnBtn : expandBtn}
+          alt={openned ? "Turn" : "Expand"}
+          onClick={() => setOpenned(!openned)}
+        />
+        <input
+          className={styles.text}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          onBlur={(e) => editTodoTitle(id, e.target.value, store, setTitle)}
+        ></input>
+      </div>
       {date ? <div className={styles.deadline}>До {date}</div> : null}
       <div className={styles.btns}>
         <ImageButton
